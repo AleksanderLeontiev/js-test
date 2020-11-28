0345a112686970f798266ee2ec63f6c052fe7e40
aleksandr <aleksandr.leontev.n@mail.ru>
2020-11-28 19:33:05 +0300

    Add install eslint

---
+++ src/homework№1.test.js
@@ -0,0 +1,6 @@
+import { showNumber} from "./homework№1.js";
+describe("test 1st task ", () => {
+    it("test for the sum of two numbers", () => {
+        expect(showNumber(1,2)).toBe(3);
+    });
+});
