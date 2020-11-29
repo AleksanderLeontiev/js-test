32dc5b193faf6f82ad5216aaf44f07489888c8b7
aleksandr <aleksandr.leontev.n@mail.ru>
2020-11-29 10:40:48 +0300

    Install and setup prettier

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   extends: [
-    'airbnb-base',
+      "airbnb-base",
+      "prettier",
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     "import/prefer-default-export": "off",
   },
 
   plugins: ["jest"],
-
 };
